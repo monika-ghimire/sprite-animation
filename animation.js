2f403f3aaafb6451578fb5ee435e67572bf049ed
monika <monika>
2022-12-05 14:32:42 +0545

    state addeed

--- animation.js
+++ animation.js
@@ -1,18 +1,18 @@
 
 
-
 function animate() {
     //clear screen
     ctx.clearRect(0, 0, canvas_width, canvas_height)
-
+   
     //geting x and y form obj
-    let position = Math.floor(gameFrame / staggerFrame) % spriteAnimation[playerState].loc.length
+    let position = Math.floor(gameFrame / staggerFrame) %
+     spriteAnimation[playerState].loc.length
     let frameX = spritwidth * position
     let frameY = spriteAnimation[playerState].loc[position].y;
 
     ctx.drawImage(playerImage, frameX, frameY,
         spritwidth, spritheight, 0, 0, spritwidth, spritheight)
-
+       
 
     gameFrame++;
     requestAnimationFrame(animate)
--- state.js
+++ state.js
@@ -1,56 +1,59 @@
 // animation states in object
-const animationStates=[
+const animationStates = [
     {
-        name:'idle',
-         frames:7
+        name: 'idle',
+        frames: 7
     },
     {
-        name:'jump',
-         frames:7
+        name: 'jump',
+        frames: 7
     },
     {
-        name:'fall',
-         frames:7
+        name: 'fall',
+        frames: 7
     },
     {
-        name:'run',
-         frames:9
+        name: 'run',
+        frames: 9
     },
     {
-        name:'dizzy',
-         frames:11
+        name: 'dizzy',
+        frames: 11
     },
     {
-        name:'sit',
-         frames:5
+        name: 'sit',
+        frames: 5
     },
     {
-        name:'roll',
-         frames:7
+        name: 'roll',
+        frames: 7
     },
     {
-        name:'bite',
-         frames:7
+        name: 'bite',
+        frames: 7
     },
     {
-        name:'ko',
-         frames:12
+        name: 'ko',
+        frames: 12
     },
     {
-        name:'getHit',
-         frames:4
+        name: 'getHit',
+        frames: 4
     }
 ];
 // its give x and y state  of frame 
-animationStates.forEach((state,index)=>{
-    let frames={
-        loc:[],
+
+for (let i = 0; i < animationStates.length; i++) {
+    let frames = {
+        loc: [],
     }
-    for(let j=0;j<state.frames;j++)
-    {
-        let positionx=j*spritwidth
-        let positiony=index*spritheight
-        frames.loc.push({x:positionx,y:positiony})
+let state=animationStates[i]
+    for (let j = 0; j < state.frames; j++) {
+        let positionx = j * spritwidth
+        let positiony = i* spritheight
+        frames.loc.push({ x: positionx, y: positiony })
     }
-    spriteAnimation[state.name]=frames;
-});
+    spriteAnimation[state.name] = frames;
+
+}
+
